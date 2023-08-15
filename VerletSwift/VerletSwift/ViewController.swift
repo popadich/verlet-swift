ddccd0a3cea04d9ecd0d44492d15d59caf9e1509
Alex Popadich <popadich@gmail.com>
2023-08-15 11:59:27 -0700

    Add actions and outlets for control buttons and display an appropriate message when they are pressed.

--- VerletSwift/VerletSwift/ViewController.swift
+++ VerletSwift/VerletSwift/ViewController.swift
@@ -9,6 +9,13 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var timeLeftField: NSTextField!
+    @IBOutlet weak var startButton: NSButton!
+    @IBOutlet weak var stopButton: NSButton!
+    @IBOutlet weak var resetButton: NSButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,6 +28,17 @@ class ViewController: NSViewController {
         }
     }
 
-
+    @IBAction func startButtonClicked(_ sender: NSButton) {
+        timeLeftField.stringValue = "Start"
+    }
+    
+    @IBAction func stopButtonClicked(_ sender: NSButton) {
+        timeLeftField.stringValue = "Stop"
+    }
+    
+    @IBAction func resetButtonClicked(_ sender: NSButton) {
+        timeLeftField.stringValue = "Reset"
+    }
+    
 }
 
