1310ec983fd5d718bc2ac1d2f6888152be9d5d10
Alex Popadich <popadich@gmail.com>
2023-08-15 11:40:16 -0700

    Create initial App project in Xcode

---
+++ VerletSwift/VerletSwift/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  VerletSwift
+//
+//  Created by Alex Popadich on 8/15/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
