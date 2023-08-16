c10a1196ff5991a90d39fddf4a50a713a26b34d9
Alex Popadich <popadich@gmail.com>
2023-08-16 07:30:51 -0700

    Create a preferences UI and add actions and outlets for elements.

---
+++ VerletSwift/VerletSwift/PrefsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  PrefsViewController.swift
+//  VerletSwift
+//
+//  Created by Alex Popadich on 8/15/23.
+//
+
+import Cocoa
+
+class PrefsViewController: NSViewController {
+
+    @IBOutlet weak var presetsPopup: NSPopUpButton!
+    @IBOutlet weak var customSlider: NSSlider!
+    @IBOutlet weak var customTextField: NSTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+    @IBAction func popupValueChanged(_ sender: Any) {
+    }
+    
+    @IBAction func sliderValueChanged(_ sender: Any) {
+    }
+    
+    @IBAction func cancleButtonClicked(_ sender: Any) {
+    }
+    
+    @IBAction func okButtonClicked(_ sender: Any) {
+    }
+    
+}
